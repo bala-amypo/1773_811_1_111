e631a87ce80df5f84a0dca9a8943a67a853ddaf8
bala-amypo <bala@amypo.in>
2025-12-26 16:35:16 +0000

    User commit

--- src/main/java/com/example/demo/service/CompatibilityScoreService.java
+++ src/main/java/com/example/demo/service/CompatibilityScoreService.java
@@ -15,20 +15,3 @@ public interface CompatibilityScoreService {
 
     List<CompatibilityScoreRecord> getAllScores();
 }
-// CompatibilityScoreService.java
-package com.example.demo.service;
-
-import com.example.demo.model.CompatibilityScoreRecord;
-
-import java.util.List;
-
-public interface CompatibilityScoreService {
-
-    CompatibilityScoreRecord computeScore(Long studentAId, Long studentBId);
-
-    List<CompatibilityScoreRecord> getScoresForStudent(Long studentId);
-
-    CompatibilityScoreRecord getScoreById(Long id);
-
-    List<CompatibilityScoreRecord> getAllScores();
-}
